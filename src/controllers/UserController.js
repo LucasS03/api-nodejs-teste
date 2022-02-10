b83cd073ceb8b4c62a797105dfb543e960eb8595
Lucas Sena <slsena45@gmail.com>
2022-02-10 15:55:00 -0300

    feat: recuperar senha, envio de email e validação de usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,82 @@
-var db = require('../services/config/db');
+const db = require('../services/config/db');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const tableUsers = 'users';
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "26bd9d1f849131",
+      pass: "d911b1840a093c"
+    }
+});
+
+const usePasswordHashToMakeToken = ({ password: passwordHash, id }) => {
+    const secret = `${passwordHash}-${id}`;
+    const token = jwt.sign(
+        { id },
+        secret,
+        { expiresIn: 3600 } // 1 hour
+    );
+
+    return token;
+};
+
+exports.sendPasswordResetEmail = async (req, res) => {
+    const { email } = req.params;
+    let user;
+
+    try {
+        user = await db(tableUsers).select('*').where('email', email).first();
+    } catch (error) {
+        res.status(404).json('No user with that email');
+    }
+
+    const token = usePasswordHashToMakeToken(user);
+    const mailOptions = {
+        to: email,
+        subject: 'Recuperação de Senha - Hitss On',
+        text: `Seu link de recuperação de senha: <br /> https://meulink.com/reset/${user.id}/${token}`
+    };
+
+    const sendEmail = () => {
+        transporter.sendMail(mailOptions, (err, data) => {
+            if(err) {
+                console.log(`Erro: ${err}`);
+                res.status(404).json('Erro interno.')
+            } else {
+                console.log('E-mail enviado com sucesso!', data.response);
+                res.status(200).json('sent');
+            }
+        });
+    };
+
+    sendEmail();
+}
+
+exports.receiveNewPassword = async (req, res) => {
+    const { id, token } = req.params;
+    const { password } = req.body;
+
+    const user = await db(tableUsers).select('*').where('id', id).first();
+    const secret = `${user.password}-${id}`;
+    const payload = jwt.decode(token, secret);
+
+    if(payload.id === user.id) {
+        const hash = await bcrypt.hashSync(password, 10);
+        db(tableUsers).update({ password: hash }).where({ id}).then(() => {
+            res.status(202).json('Senha atualizada!');
+        })
+    } else {
+        res.status(404).json('Usuário inválido!');
+    }
+}
+
+exports.login = (req, res) => {
+    res.status(200).send('ok');
+}
 
 exports.post = async (req, res) => {
     try {
@@ -14,14 +91,36 @@ exports.post = async (req, res) => {
         if(!user.last_name)
             res.status(400).send('Usuário com "último nome" inválido');
 
-        if(!user.email)
+        if(!user.email) {
+            res.status(400).send('Usuário com "email" inválido');
+        } else {
+            const hasUser = await db.select().table(tableUsers).where('email', user.email);
+            if(hasUser.length)
+                res.status(400).send(`E-mail "${user.email}" já cadastrado.`);
+        }
+
+        if(!user.cpf) {
+            res.status(400).send('Usuário com "CPF" inválido');
+        } else {
+            const hasUser = await db.select().table(tableUsers).where('cpf', user.cpf);
+            if(hasUser.length)
+                res.status(400).send(`CPF "${user.cpf}" já cadastrado.`);
+        }
+
+        if(!user.password)
             res.status(400).send('Usuário com "email" inválido');
 
+        const hash = bcrypt.hashSync(user.password, 10);
+        user.password = hash;
         const data = await db.insert(user).into(tableUsers);
 
         res.status(201).send({
             id: data[0],
-            ...user
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            phone: user.phone,
+            cpf: user.cpf
         });
     } catch (error) {
         res.status(500).send({ 
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -7,4 +7,11 @@ module.exports = (app) => {
 
     app.get('/users', UserController.get);
     app.get('/user/:id', UserController.getById);
+
+    // login
+    app.post('/user/login', UserController.login);
+
+    // reset password
+    app.post('/user/:email', UserController.sendPasswordResetEmail);
+    app.post('/user/reset/:id/:token', UserController.receiveNewPassword);
 };
