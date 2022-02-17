56fba4bc4cdfd38648c824fb5f07b644f1ffc726
Lucas Sena <slsena45@gmail.com>
2022-02-17 15:53:50 -0300

    feat: add stream para o vídeo

--- src/controllers/ClassController.js
+++ src/controllers/ClassController.js
@@ -1,4 +1,5 @@
-var db = require('../services/config/db');
+const db = require('../services/config/db');
+const fs = require('fs');
 const tableClasses = 'classes';
 
 exports.post = async (req, res) => {
@@ -111,6 +112,38 @@ exports.videoUpload = async (req, res) => {
     await db(tableClasses).where('id', id).first().update({ video: req.file.path });
 
     res.send('Salvo!');
+}
+
+exports.getVideoByClassId = async (req, res) => {
+    const { id } = req.params;
+    const movieFile = await db(tableClasses).where('id', id).first();
+
+    fs.stat(movieFile.video, (err, stats) => {
+        if(err) {
+            return res.status(404).end('<h1>Vídeo não encontrado :(</h1>');
+        }
+
+        const { range } = req.headers;
+        const { size } = stats;
+        const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+        const end = size - 1;
+        const chunkSize = (end - start) + 1;
+
+        res.set({
+            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'video/mp4'
+        });
+
+        res.status(206);
+
+        const stream = fs.createReadStream(movieFile.video, { start, end });
+        stream.on('open', () => stream.pipe(res));
+        stream.on('error', (streamErr) => res.end(streamErr));
+
+        return null;
+    })
 }
 
 const hasClass = async (id = '') => {
--- src/routes/ClassRoute.js
+++ src/routes/ClassRoute.js
@@ -1,7 +1,13 @@
 const ClassController = require('../controllers/ClassController');
 const upload = require('../../common');
+const fs = require('fs');
 
 module.exports = (app) => {
+    app.get('/', (req, res) => {
+        fs.readFile('./index.html', (err, html) => res.end(html));
+    });
+    app.get('/class/:id/video', ClassController.getVideoByClassId);
+
     app.post('/class', ClassController.post);
     app.put('/class/:id', ClassController.put);
     app.delete('/class/:id', ClassController.delete);
